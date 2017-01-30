5ae252ed4ccfa2cee5355d21073cd7cc83b2c0ad
humptyhumps <humperdinck@monstrarosa.com>
2017-01-29 20:27:25 -0800

    First Commit
    Upload Unity game to Git

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
